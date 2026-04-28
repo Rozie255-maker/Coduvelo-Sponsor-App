304329d3f7d402b668dd424259280f2ff509fa8a
Rozie255-maker <rpaessens@gmail.com>
2026-04-28 19:28:38 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 (function(){
   // GOOGLE APPS SCRIPT URL
-  var SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzSQcV9jpEpgnDoes3A9OLZk1nrnPdKGAzh2qZSB2Ffa7mK_2gM2KsPH6zz5T4_Ipgr/exec';
+  var SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzCW8AMmmPfL_0Ztzq4h1GvlKemmhaXTszpeoCimUQjb7YjzPttOZ97aPFuvx30d45L/exec';
   
   var chosenHm = 0, maxHm = 0;
   var selectedRate = 0;
